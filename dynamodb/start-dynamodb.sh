29cdd9897e4b55c69bb6492b288679086d0b988e
Tim Reidel <reideltj@yahoo.com>
2015-07-14 04:11:55 -0400

    adding tables

--- dynamodb/start-dynamodb.sh
+++ dynamodb/start-dynamodb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/java -Djava.library.path=/opt/DynamoDBLocal_lib -jar /opt/DynamoDBLocal.jar -dbPath /var/local 
+/usr/bin/java -Djava.library.path=/opt/DynamoDBLocal_lib -jar /opt/DynamoDBLocal.jar -sharedDb -dbPath /var/local 
