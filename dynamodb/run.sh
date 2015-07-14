e3caec6851003b00ec6d73c96920e96012128049
Tim Reidel <reideltj@yahoo.com>
2015-07-14 00:54:28 -0400

    adding base

---
+++ dynamodb/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# start the container
+docker run -p 8000:8000 reideltj/dynamodb
---
+++ dynamodb/start-dynamodb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/bin/java -Djava.library.path=/opt/DynamoDBLocal_lib -jar /opt/DynamoDBLocal.jar -dbPath /var/local 
